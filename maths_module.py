675b420e4f6202e16cae0138cc22624feb6e991f
zahid0247 <zahid.mirza4702@gmail.com>
2025-12-11 12:01:52 +0530

    Maths_module added

---
+++ maths_module.py
@@ -0,0 +1,10 @@
+import math
+number = float(input("Enter the number: "))
+print()
+squarte_root = math.sqrt(number)
+log = math.log(number)
+sine = math.sin(number)
+print(f"Square Root of number {number} is: {squarte_root} \n")
+print(f"Logaritham of number {number} is: {log} \n")
+print(f"Sine of number {number} is: {sine} \n")
+
